3ea56cf665680c074bddc98fd8e2ab0305556a6e
XXX Ahana <ahanasi@seas.upenn.edu>
2022-06-20 23:46:13 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { storage } from "./index";
+
+const App = () => {
+  return (
+    <div
+      style={{
+        backgroundImage: `url("https://via.placeholder.com/500")`,
+      }}
+    >
+      Hello World
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBc5ReqwYD11gd8Y-0S06KLd_K5_OEM0Fk",
+  authDomain: "wheres-waldo-c76a2.firebaseapp.com",
+  projectId: "wheres-waldo-c76a2",
+  storageBucket: "wheres-waldo-c76a2.appspot.com",
+  messagingSenderId: "887519118543",
+  appId: "1:887519118543:web:642a99ed1f641d1b3be3ba",
+  measurementId: "G-6DE7CK21JN",
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+const analytics = getAnalytics(app);
+
+export { storage };
