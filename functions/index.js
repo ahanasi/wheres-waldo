74871f42209d18f484138db0f551ccb4ff704327
XXX Ahana <ahanasi@seas.upenn.edu>
2022-06-22 15:41:19 -0600

    Initialize firebase and cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,29 @@
+// The Firebase Admin SDK to access Firestore.
+
+const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+});
+
+const db = admin.firestore();
+
+//Populate db
+const docRef = db.collection("easy").doc("waldo");
+docRef.set({
+  x_min: 1218,
+  x_max: 1248,
+  y_min: 581,
+  y_max: 611,
+});
+
+//Get Coordinates
+exports.helloHttp = functions.https.onRequest(async (req, res) => {
+  try {
+    const waldoCoords = await db.collection("easy").doc("waldo").get();
+    res.send(waldoCoords.data());
+  } catch (error) {
+    console.log("Error getting document", error);
+  }
+});
